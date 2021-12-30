29d363c32b7b2c8227fde425e7852a24273ff885
Mnishek <shavyrina.marina@mail.ru>
2021-12-30 15:44:09 +0300

    dz_3

--- webpack.config.js
+++ webpack.config.js
@@ -3,18 +3,42 @@ const HTMLWebpackPlugin = require('html-webpack-plugin')
 const {CleanWebpackPlugin} = require('clean-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
+const TerserPlugin = require("terser-webpack-plugin")
+
+const isDev = process.env.NODE_ENV === 'develpment'
+const isProd = !isDev
+
+const optimization = () => {
+  let config={}
+  if (isProd) {
+    config.minimizer = [
+      new CssMinimizerPlugin(),
+      new TerserPlugin()
+    ]
+  }
+  return config
+}
+
 
 module.exports = {
   context: path.resolve(__dirname, 'src'),
   mode: 'development',
-  entry: './js/index.js',
+  // entry: './js/index.js',
+  entry: {
+    main:['@babel/polyfill', './js/index.js']
+  },
   output: {
     filename: '[name].[hash].js',
     path: path.resolve(__dirname, 'dist')
   },
+  optimization: optimization(),
   plugins: [
     new HTMLWebpackPlugin({
-      template: './index.html'
+      template: './index.html',
+      minify: {
+        collapseWhitespace: isProd
+      }
     }),
     new CleanWebpackPlugin(),
     new CopyWebpackPlugin({
@@ -26,7 +50,7 @@ module.exports = {
       ]
     }),
     new MiniCssExtractPlugin({
-      filename: 'css/[name].[hash].css'
+      filename: './css/style.css'
     })
   ],
   module: {
@@ -38,6 +62,16 @@ module.exports = {
       {
         test: /\.(png|jpg|svg|gif|mp3)$/i,
         type: 'asset/resource'
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
       }
     ]
   }
